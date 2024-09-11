bba45b2646fd20112841bcef51102217225c276a
a1Thiago~ <a1thiago@hotmail.com>
2024-09-11 16:48:34 -0300

    end task Somatório Progressivo

--- src/app/desafio-1/page.tsx
+++ src/app/desafio-1/page.tsx
@@ -8,7 +8,7 @@ import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
 export default function Page() {
 
 
-  const desafio = `
+  const desafio1 = `
   int INDICE = 13, SOMA = 0, K = 0;
   Enquanto K < INDICE faça {
     K = K + 1;
@@ -16,7 +16,16 @@ export default function Page() {
   }
   Imprimir(SOMA);`
 
-  const resposta = `
+  const desafio2 = `
+  int INDICE = 12, SOMA = 0, K = 1;
+  Enquanto K < INDICE faça {
+    K = K + 1;
+    SOMA = SOMA + K;
+  }
+  Imprimir(SOMA);`
+
+  const resposta1 = `
+//Resolução em javascript
 let INDICE = 13
 let SOMA = 0
 let K = 0
@@ -28,12 +37,26 @@ while(K < INDICE){
 
 console.log(SOMA)
 
-91
+${cumulativeSum(13, 0, 0)}
 `
+  const resposta2 = `
+//Resolução em javascript
+let INDICE = 12
+let SOMA = 0
+let K = 1
 
+while(K < INDICE){
+  K = K + 1
+  SOMA = SOMA + K
+}
+
+console.log(SOMA)
+
+${cumulativeSum(12, 0, 1)}
+`
   return (
 
-    <div >
+    <div className='space-y-4'>
 
       <div className='flex justify-between py-4'>
         <Link href='/'>
@@ -51,31 +74,78 @@ console.log(SOMA)
         </Link>
       </div>
 
-      <h2 className='font-bold text-xl py-2'>Desafio 1</h2>
+      <h2 className='font-bold text-xl'>Somatório Progressivo</h2>
 
-      <div className='[&_p]:font-semibold space-y-2'>
+      <div>
 
-        <p>
-          Observe o trecho de código abaixo:
-        </p>
-        <SyntaxHighlighter language="javascript" style={dracula}>
-          {desafio}
-        </SyntaxHighlighter>
-        <p>
-          Ao final do processamento, qual será o valor da variável SOMA?
-        </p>
+        <h3 className='font-bold text-lg'>Desafio 1</h3>
 
-        <SyntaxHighlighter language="javascript" style={dracula}>
-          {resposta}
-        </SyntaxHighlighter>
+        <div className='[&_p]:font-semibold space-y-2'>
+
+          <p>
+            Observe o trecho de código abaixo:
+          </p>
+          <SyntaxHighlighter language="javascript" style={dracula}>
+            {desafio1}
+          </SyntaxHighlighter>
+          <p>
+            Ao final do processamento, qual será o valor da variável SOMA?
+          </p>
+
+          <SyntaxHighlighter language="javascript" style={dracula}>
+            {resposta1}
+          </SyntaxHighlighter>
+
+        </div>
+
+
+        <div>
+          <p className='font-semibold py-2'>Resposta: {cumulativeSum(13, 0, 0)}</p>
+        </div>
+
 
       </div>
 
-
       <div>
-        <p className='font-semibold py-2'>Resposta: 91</p>
+
+        <h3 className='font-bold text-lg'>Desafio 2</h3>
+
+        <div className='[&_p]:font-semibold space-y-2'>
+
+          <p>
+            Observe o trecho de código abaixo:
+          </p>
+          <SyntaxHighlighter language="javascript" style={dracula}>
+            {desafio2}
+          </SyntaxHighlighter>
+          <p>
+            Ao final do processamento, qual será o valor da variável SOMA?
+          </p>
+
+          <SyntaxHighlighter language="javascript" style={dracula}>
+            {resposta2}
+          </SyntaxHighlighter>
+
+        </div>
+
+
+        <div>
+          <p className='font-semibold py-2'>Resposta: {cumulativeSum(12, 0, 1)}</p>
+        </div>
+
+
       </div>
 
     </div>
   )
+}
+
+function cumulativeSum(indice: number, soma: number, k: number) {
+
+  while (k < indice) {
+    k = k + 1
+    soma = soma + k
+  }
+
+  return soma
 }
